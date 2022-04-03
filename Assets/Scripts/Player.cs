96f89e345df156ca0d58293bcb75c6f79967b497
Kacper Grela <38227679+kgrela@users.noreply.github.com>
2022-04-04 00:30:44 +0200

    Added sprites

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocity = 1;
+    public Rigidbody2D rb;
+    public GameObject wall;
+    public SpriteRenderer s;
+
     void Start()
     {
-        
+        // rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+     
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(wall.CompareTag("wall"))
+        {
+            s.flipX = true;
+        }
+    }
+
+
 }
